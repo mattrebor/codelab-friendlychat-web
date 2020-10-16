e08dd4812dc5b74672f50eb0e2ffaacd190bbac4
Robert Tam <robert.tam+git@gmail.com>
2020-10-16 14:06:32 -0400

    Initial Functions implementation

--- web-start/functions/index.js
+++ web-start/functions/index.js
@@ -18,8 +18,80 @@
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
+    console.log('A new user signed in for the first time.');
+    const fullName = user.displayName || 'Anonymous';
+
+    await admin.firestore().collection('messages').add({
+        name: 'Firebase Bot',
+        profilePicUrl: '/images/firebase-logo.png',
+        text: `${fullName} signed in for the first time! Welcome!`,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    console.log('Welcome message written to database.');
+});
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
+
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+const spawn = require('child-process-promise').spawn;
+
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+exports.blueOffensiveImages = functions.runWith({memory: '2GB'}).storage.object().onFinalize(
+    async(object) => {
+        const req = {
+            image: {
+                source: {imageUri: `gs://${object.bucket}/${object.name}`},
+            },
+            features: [
+                {type: "SAFE_SEARCH_DETECTION"}
+            ]
+        };
+
+        console.log("Processing: ", req);
+        const [result] = await client.annotateImage(req);
+        const safeSearchResult = result.safeSearchAnnotation;
+        //const Likelihood = vision.Likelihood;
+        if (safeSearchResult.violence == 'LIKELY' || safeSearchResult.violence == 'VERY_LIKELY') {
+            
+            console.log('The image ', object.name, ' has been detected as inappropriate.');
+            return blurImage(object.name);
+        }
+        console.log('The image ', object.name, ' has been detected as OK.');
+            
+     });
+
+// Blurs the given image located in the given bucket using ImageMagick.
+async function blurImage(filePath) {
+    const tempLocalFile = path.join(os.tmpdir(), path.basename(filePath));
+    const messageId = filePath.split(path.sep)[1];
+    const bucket = admin.storage().bucket();
+  
+    // Download file from bucket.
+    await bucket.file(filePath).download({destination: tempLocalFile});
+    console.log('Image has been downloaded to', tempLocalFile);
+    // Blur the image using ImageMagick.
+    await spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x24', tempLocalFile]);
+    console.log('Image has been blurred');
+    // Uploading the Blurred image back into the bucket.
+    await bucket.upload(tempLocalFile, {destination: filePath});
+    console.log('Blurred image has been uploaded to', filePath);
+    // Deleting the local file to free up disk space.
+    fs.unlinkSync(tempLocalFile);
+    console.log('Deleted local file.');
+    // Indicate that the message has been moderated.
+    await admin.firestore().collection('messages').doc(messageId).update({moderated: true});
+    console.log('Marked the image as moderated in the database.');
+}
+
 
 // TODO(DEVELOPER): Write the sendNotifications Function here.
